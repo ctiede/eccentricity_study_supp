d914c625c6443ed876563092412f9e55cac6af68
Chris <cwt271@nyu.edu>
2020-07-29 11:05:44 -0500

    outputs strobed-time-average for each mean anomaly

--- time_average_reductions.py
+++ time_average_reductions.py
@@ -10,63 +10,89 @@ from eccentricity_study import *
 
 
 
-parser = ArgumentParser()
-parser.add_argument("filenames", nargs='+', help="set of stacked_reduction.h5 files to time average")
-args = parser.parse_args()
-
-for f in args.filenames:
-    print(f)
-    h5f = h5py.File(f, 'r') 
-
-    # Scalar
-    e = h5f['eccentricity'][...]
-    n = h5f['radial_bins' ][...]
-    # m = h5f['bins_2d'][...]
-
-    # 1D arrays
-    M = h5f['mean_anomaly'     ][...]
-    E = h5f['eccentric_anomaly'][...]
-    f = h5f['true_anomaly'     ][...]
-    em = h5f['sigma_moment'][...]
-    ev = h5f['vr_moment'   ][...]
-
-    # 2D arrays
-    s = h5f['sigma'      ][...]
-    p = h5f['work_on'    ][...]
-    t = h5f['torque_on'  ][...]
-
-    # 3D maps
-    S = h5f['remapped_sigma'][...]
-    T = h5f['remapped_Ldot' ][...]
-    P = h5f['remapped_Edot' ][...]
-
-    avg_sigma_moment = np.mean(em)
-    avg_vr_moment    = np.mean(ev)
-
-    avg_sigma_profile = np.mean(s, axis=0)
-    avg_work_profile  = np.mean(p, axis=0)
-    avg_torq_profile  = np.mean(t, axis=0)
-
-    avg_sigma_map = np.mean(S, axis=2)
-    avg_Ldot_map  = np.mean(T, axis=2)
-    avg_Edot_map  = np.mean(P, axis=2)
-
-    if e < 0.1:
-        ecc = str(e).split('.')[-1]
+def write_strobed_to_group(group, data, dim):
+    if dim == 1:
+        group['Periapse'] = np.mean(data[7::8])
+        group['pi_4'    ] = np.mean(data[0::8])
+        group['pi_2'    ] = np.mean(data[1::8])
+        group['3pi_4'   ] = np.mean(data[2::8])
+        group['Apoapse' ] = np.mean(data[3::8])
+        group['5pi_4'   ] = np.mean(data[4::8])
+        group['3pi_2'   ] = np.mean(data[5::8])
+        group['7pi_4'   ] = np.mean(data[6::8])
+    elif dim == 2:
+        group['Periapse'] = np.mean(data[7::8], axis=0) 
+        group['pi_4'    ] = np.mean(data[0::8], axis=0) #The first output from relevant data is at pi / 4
+        group['pi_2'    ] = np.mean(data[1::8], axis=0)
+        group['3pi_4'   ] = np.mean(data[2::8], axis=0)
+        group['Apoapse' ] = np.mean(data[3::8], axis=0)
+        group['5pi_4'   ] = np.mean(data[4::8], axis=0)
+        group['3pi_2'   ] = np.mean(data[5::8], axis=0)
+        group['7pi_4'   ] = np.mean(data[6::8], axis=0)
+    elif dim == 3:
+        group['Periapse'] = np.mean(data[:, :, 7::8], axis=2) 
+        group['pi_4'    ] = np.mean(data[:, :, 0::8], axis=2)
+        group['pi_2'    ] = np.mean(data[:, :, 1::8], axis=2)
+        group['3pi_4'   ] = np.mean(data[:, :, 2::8], axis=2)
+        group['Apoapse' ] = np.mean(data[:, :, 3::8], axis=2)
+        group['5pi_4'   ] = np.mean(data[:, :, 4::8], axis=2)
+        group['3pi_2'   ] = np.mean(data[:, :, 5::8], axis=2)
+        group['7pi_4'   ] = np.mean(data[:, :, 6::8], axis=2)
     else:
-        ecc = str(int(e * 1e3))
-    output_fname = 'time_avg_reductions_e{}.h5'.format(ecc)
+        print("Invalid (dim)")
 
-    print('   writing', output_fname)
-    h5w = h5py.File(output_fname, 'w')
-    h5w['eccentricity']   = e
-    h5w['radial_bins']    = n
-    h5w['sigma_moment']   = avg_sigma_moment
-    h5w['vr_moment']      = avg_vr_moment
-    h5w['sigma']          = avg_sigma_profile
-    h5w['work_on']        = avg_work_profile
-    h5w['torque_on']      = avg_torq_profile
-    h5w['remapped_sigma'] = avg_sigma_map
-    h5w['remapped_Ldot']  = avg_Ldot_map
-    h5w['remapped_Edot']  = avg_Edot_map
-    h5w.close()
+
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("filenames", nargs='+', help="set of stacked_reduction.h5 files to time average")
+    args = parser.parse_args()
+
+    for f in args.filenames:
+        print(f)
+        h5f = h5py.File(f, 'r') 
+
+        # Scalar
+        e = h5f['eccentricity'][...]
+        n = h5f['radial_bins' ][...]
+        m = h5f['bins_2d'][...]
+
+        # 1D arrays
+        M = h5f['mean_anomaly'     ][...]
+        E = h5f['eccentric_anomaly'][...]
+        f = h5f['true_anomaly'     ][...]
+        em = h5f['sigma_moment'][...]
+        ev = h5f['vr_moment'   ][...]
+
+        # 2D arrays
+        s = h5f['sigma'      ][...]
+        p = h5f['work_on'    ][...]
+        t = h5f['torque_on'  ][...]
+
+        # 3D maps
+        S = h5f['remapped_sigma'][...]
+        T = h5f['remapped_Ldot' ][...]
+        P = h5f['remapped_Edot' ][...]
+
+
+        if e < 0.1:
+            ecc = str(e).split('.')[-1]
+        else:
+            ecc = str(int(e * 1e3))
+        output_fname = 'time_avg_reductions_e{}.h5'.format(ecc)
+
+        print('   writing', output_fname)
+        h5w = h5py.File(output_fname, 'w')
+        h5w.create_dataset('eccentricity', data=e)
+        h5w.create_dataset('radial_bins' , data=n)
+        h52.create_dataset('bins_2d'     , data=m)
+        write_strobed_to_group(h5w.create_group('sigma_moment')  , M, em, 1)
+        write_strobed_to_group(h5w.create_group('vr_moment')     , M, ev, 1)
+        write_strobed_to_group(h5w.create_group('sigma')         , M, s , 2)
+        write_strobed_to_group(h5w.create_group('work_on')       , M, p , 2)
+        write_strobed_to_group(h5w.create_group('torque_on')     , M, t , 2)
+        write_strobed_to_group(h5w.create_group('remapped_sigma'), M, S , 3)
+        write_strobed_to_group(h5w.create_group('remapped_Ldot') , M, T , 3)
+        write_strobed_to_group(h5w.create_group('remapped_Edot') , M, P , 3)
+        h5w.close()
